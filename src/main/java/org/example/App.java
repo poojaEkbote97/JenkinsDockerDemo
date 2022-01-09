1b1e03778b35677d85dc5f1b6d48f5428dd6e280
Pooja <poojaekbote97@gmail.com>
2022-01-09 10:27:19 +0530

    femo project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class App {
+	public static boolean checkIfInputIsAnEvenNumber(int number)
+	{
+		return number % 2==0;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+	}
+
+}
---
+++ src/main/java/org/example/AppTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class AppTest {
+    public static void main(String[] args) {
+		
+	}
+
+	
+}
